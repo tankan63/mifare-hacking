d8dc255b5d537fff112c13596c0ac8968d3a995f
Tanay <tanay@allthenticate.com>
2024-07-25 09:47:48 -0500

    upodated source scrript

--- src/read_card.py
+++ src/read_card.py
@@ -1,7 +1,24 @@
 import os
 
 def main():
-    print("Hello world")
+    while True:
+        # Read input from user
+        print("-"*25)
+        user_input = input("What would you like to do with your MiFare card? : ")
+        print("-"*25)
+
+        if user_input == "help":
+            print("Commands: \n"
+                  "1. info - Get some details about your card\n"
+                  "2. read - Read the data from your card\n"
+                  "3. crack - Unlock the data from the sectors on your card\n"
+                  "4. help - Show this help message\n"
+                  "5. exit - Exit the program\n")
+        elif user_input == "info":
+            # Run nfc-poll to get the card details and print the output
+            os.system("nfc-poll -v ")
+        elif user_input == "exit":
+            break
 
 if __name__ == "__main__":
-    main()
+    main(
