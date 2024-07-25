6d4ceb37bc00a70814ca5454118cddf51375df10
Tanay Mehra <mehra.tanay@gmail.com>
2024-07-25 18:50:49 -0500

    some error handling

--- src/read_card.py
+++ src/read_card.py
@@ -8,7 +8,7 @@ def main():
         user_input = input("What would you like to do with your MiFare card? : ")
         print("-" * 25)
 
-        if user_input == "help":
+        if user_input.lower() == "help":
             print("Commands: \n"
                   "1. info - Get some details about your card\n"
                   "2. read - Read the data from your card\n"
@@ -19,6 +19,16 @@ def main():
             # Run nfc-poll to get the card details and print the output
             os.system("nfc-poll -v ")
         elif user_input == "exit":
+            break
+        else:
+            print("Invalid command! Refer to the list below for your options:\n")
+            print("Commands: \n"
+                  "1. info - Get some details about your card\n"
+                  "2. read - Read the data from your card\n"
+                  "3. crack - Unlock the data from the sectors on your card\n"
+                  "4. help - Show this help message\n"
+                  "5. exit - Exit the program\n")
+
             break
 
 
